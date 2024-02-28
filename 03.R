50f174f7745a9c27c0d7a0a12b1a73ef732d327d
E.GOPI <122505747+Egopi-1234@users.noreply.github.com>
2024-02-28 19:15:49 +0530

    Update 03.R

--- 03.R
+++ 03.R
@@ -1,8 +1,11 @@
-#Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers 
-#from 20 to 60 and sum of numbers from 51 to 91.
-print("sequence of number from 20 to 50")
-print(seq(20,50))
-print("mean of number from 30 to 60")
-print(seq(20,60))
-print("sum of number from 51 to 91")
-print(seq(51,91))
+# Print sequence of numbers from 20 to 50
+cat("Sequence of numbers from 20 to 50:\n")
+print(seq(20, 50))
+
+# Print mean of numbers from 20 to 60
+cat("Mean of numbers from 20 to 60:\n")
+print(mean(20:60))
+
+# Print sum of numbers from 51 to 91
+cat("Sum of numbers from 51 to 91:\n")
+print(sum(51:91))
